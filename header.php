1aa5acddabf1a2b5c9eb80d15f06fd1fd6adb3d6
mathieuradjouki <m.radjouki@codeur.online>
2020-07-13 22:55:45 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <title>Portfolio Mathieu RADJOUKI</title>
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+     <link rel="stylesheet" href="main.css">
+  </head>
+  <body>
+    <header>
+      <!--Images fond-->
+        <img src="images/header.jpg" alt="fond-header" class="header-bg">
+        <img src="images/logo.png" alt="logo" class="main-logo">
+    </header>

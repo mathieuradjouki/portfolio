41b30041271b71ff0516a83df1e45b51bc775947
mathieuradjouki <m.radjouki@codeur.online>
2020-07-15 14:28:11 +0200

    ajout formulaire de contact dans footer

--- footer.php
+++ footer.php
@@ -1,4 +1,35 @@
-
+<section class="contact p-5">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+      <h2 class="text-center">Pour plus d'information n'hésitez pas à me contacter</h2>
+      </div>
+      <div class="row w-100 m-0">
+        <!--Form contact-->
+        <div class="col-12">
+          <form class="contact" name="contact" method="post">
+            <div class="form-group">
+              <label for="contact_name">Name</label>
+              <input type="text" placeholder="Antoine DUPONT" name="" id="contact_name" class="form-control input-contact">
+            </div>
+            <div class="form-group">
+              <label for="contact_email">Email</label>
+              <input type="text" placeholder="monemail@email.com" name="" id="contact_email"class="form-control input-contact">
+            </div>
+            <div class="form-group">
+              <label for="contact_object">Object</label>
+              <input type="text" name="" class="form-control input-contact" id="contact_object">
+            </div>
+            <div class="form-group">
+              <label for="contact_message">Message</label>
+              <textarea name="" rows="8" class="form-control input-contact"></textarea>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>

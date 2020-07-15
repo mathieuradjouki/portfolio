69c23a933622b2c56cbe083784d3d7c2cbf1f470
mathieuradjouki <m.radjouki@codeur.online>
2020-07-15 09:48:00 +0200

    add new file

--- header.php
+++ header.php
@@ -6,10 +6,11 @@
      <title>Portfolio Mathieu RADJOUKI</title>
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
      <link rel="stylesheet" href="main.css">
+     <script src="main.js"></script>
   </head>
   <body>
     <header>
       <!--Images fond-->
         <img src="images/header.jpg" alt="fond-header" class="header-bg">
-        <img src="images/logo.png" alt="logo" class="main-logo">
+        <img src="images/logo.png" id="logo-header" onmouseover="openLogo" alt="logo" class="main-logo">
     </header>

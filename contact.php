7e756c4d421e01fe27eeef263971fdfecf98dc2d
mathieuradjouki <m.radjouki@codeur.online>
2020-07-19 13:44:49 +0200

    verification php ajout de message erreur et succes

--- contact.php
+++ contact.php
@@ -5,8 +5,8 @@ $errors = [];
  if(!isset($_POST['name'])) {
     $errors['name'] = "vous n'avez pas renseigné votre nom";
  }
- if(!isset($_POST['mail'])) {
-    $errors['mail'] = "vous n'avez pas renseigné votre email";
+ if(!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    $errors['email'] = "Votre email n'est pas valide";
  }
  if(!isset($_POST['object'])) {
     $errors['object'] = "vous n'avez pas renseigné d'objet";
@@ -14,16 +14,16 @@ $errors = [];
  if(!isset($_POST['message'])) {
     $errors['message'] = "vous n'avez pas écrit votre message";
  }
- if(!filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)){
-   $errors['mail'] = "vous n'avez pas renseigné votre email";
- }
+ session_start();
  if(!empty($errors)){
-
    $_SESSION['errors'] = $errors;
-   header('location: index.php');
+   $_SESSION['input'] = $_POST;
+   header('location: index.php#contact');
  }else{
+   $_SESSION['succes'] = '1';
    $message = $_POST['message'];
    $headers = 'FROM: siteportfolio@codeur.online';
-   echo $_POST['mail'];
-   //mail('m.radjouki@codeur.online', 'formulaire de contact', $message, $headers);
+   echo $_POST['email'];
+   mail('m.radjouki@codeur.online', 'formulaire de contact', $message, $headers);
+   header('location: index.php#contact');
  }
--- footer.php
+++ footer.php
@@ -13,25 +13,30 @@
               <?= implode("<br>",$_SESSION['errors']); ?>
             </div>
             <?php endif; ?>
+            <?php if(isset($_SESSION['succes'])): ?>
+              <div class="alert alert-succes">
+                Votre email a été envoyé avec succés!
+              </div>
+            <?php endif; ?>
           <form class="contact" action="contact.php" name="contact" method="POST" onsubmit="return validForm()">
             <div class="form-group">
-              <label for="contact_name">Name</label>
-              <input type="text" placeholder="Antoine DUPONT" name="name" id="contact_name" class="form-control input-contact" required pattern="[A-Za-z' -]+" maxlength="25">
+              <label for="contact_name">Nom</label>
+              <input type="text" placeholder="Prenom NOM" name="name" id="contact_name" class="form-control input-contact" required pattern="[A-Za-z' -]+" maxlength="25" value="<?= isset($_SESSION['input']['name']) ? $_SESSION['input']['name'] : '';?>">
             </div>
             <div class="form-group">
               <label for="contact_email">E-mail</label>
-              <input type="text" placeholder="monemail@email.com" name="email" id="contact_email"class="form-control input-contact" required>
+              <input type="email" placeholder="monemail@email.com" name="email" id="contact_email"class="form-control input-contact" required value="<?= isset($_SESSION['input']['email']) ? $_SESSION['input']['email'] : '';?>">
             </div>
             <div class="form-group">
               <label for="contact_object">Objet</label>
-              <input type="text" name="object" class="form-control input-contact" id="contact_object" required pattern="[A-Za-z' -]+">
+              <input type="text" name="object" class="form-control input-contact" id="contact_object" required pattern="[A-Za-z' -]+" value="<?= isset($_SESSION['input']['object']) ? $_SESSION['input']['object'] : '';?>">
             </div>
             <div class="form-group">
               <label for="contact_message">Message</label>
-              <textarea name="message" rows="8" id="contact_message" class="form-control input-contact" required></textarea>
+              <textarea name="message" rows="8" id="contact_message" class="form-control input-contact" required><?= isset($_SESSION['input']['message']) ? $_SESSION['input']['message'] : '';?></textarea>
             </div>
             <div class="form-group">
-              <button type="submit" id=contact_send>Envoyer message</button>
+              <button type="submit" class="btn btn-outline-dark" id="contact_send">Envoyer message</button>
             </div>
           </form>
         </div>
@@ -47,4 +52,6 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
   </body>
 </html>
+<?php unset($_SESSION['input']); ?>
+<?php unset($_SESSION['succes']); ?>
 <?php unset($_SESSION['errors']); ?>

a21ebc77b0a8baddb5f5c0a4657ea749a984fb3d
mathieuradjouki <m.radjouki@codeur.online>
2020-09-25 12:55:26 +0200

    index.php

--- index.php
+++ index.php
@@ -21,15 +21,43 @@
   <section class="projet">
     <div class="container">
       <h2>Mes Projets</h2>
-      <div id="carrousel">
-        <div class="hide-left">
-
-        </div>
-        <div class="prev-left">
+        <!-- <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="images/concession.png" alt="image du site concession">
+            </div>
+            <div class="carousel-item">
+              <img src="images/hiking.jpg" alt="image du site hiking">
+            </div>
+            <div class="carousel-item">
+            <img src="images/office.png" alt="image du site office du tourisme">
+            </div>
+          </div>
+        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div> -->
+      <!-- <div id="carrousel">
+        <div id="prev-left">
           <img src="images/concession.png" alt="image du site concession">
         </div>
+        <div id="selected">
+          <img src="images/hiking.jpg" alt="image du site hiking">
+        </div>
+        <div id="next-right">
+          <img src="images/office.png" alt="image du site office du tourisme">
+        </div>
       </div>
+      <div class="button">
+      <button type="button" name="button" id="prev">prev</button>
+      <button type="button" name="button" id="next">next</button>
     </div>
+    </div> -->
   </section>
 
 <?php include'footer.php'?>

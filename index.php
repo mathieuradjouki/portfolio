e93dd49cc7757914f61181d17f81eaeda90b820e
mathieuradjouki <m.radjouki@codeur.online>
2021-01-16 23:29:22 +0100

    update project

--- index.php
+++ index.php
@@ -2,7 +2,34 @@
  ?>
     <div class="projects">
         <div class="project-sign">
-            <img src="assets/images/sign.png" alt="sign project">
+            <img src="assets/images/project-sign.png" alt="sign project">
+        </div>
+        <div class="row row-cols-1 row-cols-md-2 g-4 project-cards">
+            <div class="col">
+                <div class="card">
+                    <img src="assets/images/mockup-hiking.jpg" class="card-img-top" alt="mockup maquette hiking project">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="assets/images/mockup-wp.jpg" class="card-img-top" alt="mockup site les écarts de rémunérations">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="assets/images/concession.jpg" class="card-img-top" alt="mockup site concession auto">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="assets/images/off-tourisme-mockup.png" class="card-img-top" alt="maquette refonte de site office du tourisme">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="myself">
+        <div class="self-sign">
+            <img src="assets/images/me-sign.png" alt="myself sign">
         </div>
     </div>
  <?php include'footer.php'

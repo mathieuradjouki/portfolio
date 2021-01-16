1a47c8bf619171702704c9426918aa489f0f7bb5
mathieuradjouki <m.radjouki@codeur.online>
2021-01-16 20:49:03 +0100

    update header and style

--- header.php
+++ header.php
@@ -4,7 +4,8 @@
   <head>
     <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-     <title>Portfolio Mathieu RADJOUKI</title>
+     <meta name="description" content="Ceci est le portfolio Web design et Développement Web, Web Mobile de Mathieu RADKJOUKI. N'hésitez pas à me contacter à l'aide du formulaire ou des reseau sociaux pour en savoir plus">
+     <title>Portfolio Web Designer, Web developpeur Mathieu RADJOUKI</title>
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
      <link rel="stylesheet" href="assets/css/style.css">
   </head>

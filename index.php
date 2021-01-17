406b52bc4cc40697d7b804e5531d044268dde0b5
mathieuradjouki <m.radjouki@codeur.online>
2021-01-17 12:27:52 +0100

    add presentation section

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
         <div class="row row-cols-1 row-cols-md-2 g-4 project-cards">
             <div class="col">
                 <div class="card">
-                    <img src="assets/images/mockup-hiking.jpg" class="card-img-top" alt="mockup maquette hiking project">
+                    <img src="assets/images/test.png" class="card-img-top" alt="mockup maquette hiking project">
                 </div>
             </div>
             <div class="col">
                 <div class="card">
-                    <img src="assets/images/mockup-wp.jpg" class="card-img-top" alt="mockup site les écarts de rémunérations">
+                    <img src="assets/images/group2.png" class="card-img-top" alt="mockup site les écarts de rémunérations">
                 </div>
             </div>
             <div class="col">
@@ -30,8 +30,24 @@
     <div class="myself">
         <div class="self-sign">
             <img src="assets/images/me-sign.png" alt="myself sign">
+        </div>  
+        <div class="presentation row">
+            <div class="text">
+                <p>Je suis Mathieu, developpeur et designer junior. Apres une belle premiere carriere, je me suis reconverti dans le web pour laisser libre court a ma passion du numerique. Ce site vous montre mon parcours.</p>
+            </div>
+            <div class="portrait">
+                <img src="assets/images/portrait.png" alt="myself in portrait">
+            </div>
         </div>
     </div>
+    <div class="parcours row">
+            <div class="parcours-sign">
+                <img src="assets/images/parcours.png" alt="parcours sign">
+            </div>
+            <div class="dates">
+                <img src="assets/images/snowfloor.png" alt="floor">
+            </div>
+        </div>
  <?php include'footer.php'
  ?>
  

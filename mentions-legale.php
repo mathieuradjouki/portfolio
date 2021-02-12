c1f7f02446c11edc26ab55e6e8901ac41b5cacb3
mathieuradjouki <mathieu.radjouki@codeur.online>
2021-02-12 14:50:26 +0100

    delete introduction in mention legale

--- mentions-legale.php
+++ mentions-legale.php
@@ -1,5 +1,4 @@
-<strong>La seule contrepartie à l'utilisation de ces mentions légales, est l'engagement total à laisser le lien crédit subdelirium sur cette page de mentions légales.</strong><br />
-Vos mentions légales :
+
 <h2>Informations légales</h2>
 <h3>1. Présentation du site.</h3>
 <p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site <a href="http://https://mathieur.promo-42.codeur.online/portfolio//">https://mathieur.promo-42.codeur.online/portfolio/</a> et <a href="http://mathieu-radjouki.fr/portfolio/">http://mathieu-radjouki.fr/portfolio/</a> l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi :</p>

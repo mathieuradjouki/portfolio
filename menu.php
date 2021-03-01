480c000f67c186667a4f30faae7e71390daf2a39
mathieuradjouki <mathieu.radjouki@codeur.online>
2021-03-01 16:16:46 +0100

    update menu, update alt tag

--- menu.php
+++ menu.php
@@ -1,17 +1,17 @@
 <!--Icone ouvrant menu-->
 <div class="open-menu" onclick="openNav()">
-        <div class="bar1"><img src="assets/images/bar.png" alt="image to open menu"></div>
-        <div class="bar2"><img src="assets/images/bar.png" alt="image to open menu"></div>
-        <div class="bar3"><img src="assets/images/bar.png" alt="image to open menu"></div>
+        <div class="bar1"><img src="assets/images/bar.png" alt="image barre d'ouverture du menu"></div>
+        <div class="bar2"><img src="assets/images/bar.png" alt="image barre d'ouverture du menu"></div>
+        <div class="bar3"><img src="assets/images/bar.png" alt="image barre d'ouverture du menu"></div>
       </div>
       <div class="menu">
           <div id="myNav" class="overlay">
       <!-- Button to close the overlay navigation -->
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">
               <div class="container">
-                <div class="close-bar1"><img src="assets/images/bar.png" alt="image to close menu"></div>
+                <div class="close-bar1"><img src="assets/images/bar.png" alt="image de fermeture du menu"></div>
                 <div class="close-bar2"></div>
-                <div class="close-bar3"><img src="assets/images/bar.png" alt="image to close menu"></div>
+                <div class="close-bar3"><img src="assets/images/bar.png" alt="image de fermeture menu"></div>
               </div>
             </a>
       <!-- Overlay content -->
